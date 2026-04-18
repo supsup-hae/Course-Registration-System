3ae4c4d69c110b837ecc6d457781ef8cca0cca12
supsup-hae <zksdbstjq@gmail.com>
2026-04-18 21:50:09 +0900

    🎉 init(#1): 초기 Spring Boot 애플리케이션 설정
    - Spring Boot 프로젝트 및 기본 구조 생성
    - PostgreSQL 및 Redis 데이터베이스 설정 추가
    - Checkstyle 정의

---
+++ src/main/java/com/liveklass/LiveklassApplication.java
@@ -0,0 +1,13 @@
+package com.liveklass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiveklassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiveklassApplication.class, args);
+	}
+
+}
