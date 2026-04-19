37ff6ca91d2f4cac7d914320dd23b81687d4f53a
supsup-hae <zksdbstjq@gmail.com>
2026-04-19 22:11:23 +0900

    ✨ feat(#7): 강의 상세 정보 조회 API 추가

---
+++ src/main/java/com/liveklass/domain/course/controller/query/CourseQueryController.java
@@ -0,0 +1,36 @@
+package com.liveklass.domain.course.controller.query;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.liveklass.common.response.BaseResponse;
+import com.liveklass.common.util.ResponseUtils;
+import com.liveklass.domain.course.dto.common.CourseInfoDto;
+import com.liveklass.domain.course.service.facade.CourseFacadeService;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+
+
+@Tag(name = "강의", description = "강의 관련 API")
+@RestController
+@RequestMapping("/api/v1/courses")
+@RequiredArgsConstructor(access = AccessLevel.PROTECTED)
+public class CourseQueryController {
+
+	private final CourseFacadeService courseFacadeService;
+
+	@Operation(summary = "강의 상세 조회", description = "강의의 상세 정보를 조회합니다.")
+	@GetMapping("/{courseId}")
+	public ResponseEntity<BaseResponse<CourseInfoDto>> getCourse(
+		@PathVariable final Long courseId
+	) {
+		CourseInfoDto response = courseFacadeService.findCourseDetail(courseId);
+		return ResponseUtils.ok(response);
+	}
+}
