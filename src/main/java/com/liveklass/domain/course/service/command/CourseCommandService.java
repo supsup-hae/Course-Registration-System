27f5bbfda578fe98cc80ea1219966f10f7f2ff13
supsup-hae <zksdbstjq@gmail.com>
2026-04-19 18:00:13 +0900

    ✨ feat(#5): 강의 등록 서비스 및 레포지토리 추가

---
+++ src/main/java/com/liveklass/domain/course/service/command/CourseCommandService.java
@@ -0,0 +1,8 @@
+package com.liveklass.domain.course.service.command;
+
+import com.liveklass.domain.course.entity.Course;
+
+public interface CourseCommandService {
+
+	Course registerCourse(Course course);
+}
